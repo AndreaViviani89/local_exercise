fa89a3ff2e02c0e7755a9613f05e70d9dfe040be
Andrea Viviani <andrea.viviani89@gmail.com>
2022-03-01 15:42:45 +0100

    First Test with PyCharm

---
+++ Hello World.py
@@ -0,0 +1,9 @@
+# Hello World Screenshot
+
+print("Hello World")
+
+character_name = "Andrea"
+character_age = "33"
+is_male = True
+print("c'era una volta un ragazzo di nome " + character_name + ", ")
+print("aveva " + character_age + " anni,")
