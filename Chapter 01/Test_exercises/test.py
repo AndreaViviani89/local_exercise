ffde0bc0c65f24f0c8b6c8274ce9eb25f379b1b4
Andrea Viviani <andrea.viviani89@gmail.com>
2022-03-23 12:44:31 +0100

    Update test.py

--- Chapter 01/Test_exercises/test.py
+++ Chapter 01/Test_exercises/test.py
@@ -1,1 +1,21 @@
 print('Hello world')
+
+
+
+
+numeros = [ 5, 6, 2, 7, 0, 10, 4]
+i = 0
+n = numeros[i]
+sum = 0
+while n:
+	sum += n 
+	i += 1
+	n = numeros[i]
+print(sum)
+
+
+
+import numpy as np
+x = np.random.random(10)
+
+print(x)
