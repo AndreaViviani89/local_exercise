0a8da481b8188fe81fd488a9ade377b945bba910
Andrea Viviani <andrea.viviani89@gmail.com>
2022-04-19 21:43:31 +0200

    Finish the file train.py - working on the preprocess in the predict.py

--- Chapter 02/07. Gradient Boosting and Encoding/predict.py
+++ Chapter 02/07. Gradient Boosting and Encoding/predict.py
@@ -3,6 +3,9 @@ import pandas as pd
 import numpy as np
 model = tr.train_model()
 
+ct = model[0]
+print(ct)
+
 while True:
 
     age = int(input("How old are you? \n"))
@@ -14,14 +17,25 @@ while True:
         sex = 'female'
 
     child = int(input("How many children do you have? \n"))
-    smoke = bool(input("Do you smoke? \n"))
+    smoke = bool(input("Do you smoke? If you smoke answer 'y', if you don't smoke answer with an 'n': \n"))
+
+    if smoke == 'y':
+        smoke = 'yes'
+    else:
+        smoke = 'no'
+
     bmi = float(input("What's is your bmi? \n"))
     region = str(input('Which is your residential area in the US? \n'))
 
     # Preprocess
 
+    # x_train = ct.fit_transform(x_train)
+    # x_test = ct.transform(x_test)
+    gbr = model[1]
 
     x = pd.DataFrame({"age":age, "sex":sex, "bmi":bmi, "child":child, "smoke":smoke," region":region}, index=[0])
+
+    x_trans = gbr.transform(x)
 
 
 
