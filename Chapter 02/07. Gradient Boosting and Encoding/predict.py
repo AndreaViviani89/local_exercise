cf5012db203e87d5e3c97a3e6919abf172216cde
Andrea Viviani <andrea.viviani89@gmail.com>
2022-04-19 16:53:13 +0200

    update

--- Chapter 02/07. Gradient Boosting and Encoding/predict.py
+++ Chapter 02/07. Gradient Boosting and Encoding/predict.py
@@ -1,13 +1,35 @@
+import train as tr
+import pandas as pd
+import numpy as np
+model = tr.train_model()
 
-model = ""
 while True:
 
     age = int(input("How old are you? \n"))
+    sex = str(input("Which is your sex? If you are a male answer 'm', if you are a female answer with an 'f': \n"))
+
+    if sex == 'm':
+        sex = 'male'
+    else:
+        sex = 'female'
+
     child = int(input("How many children do you have? \n"))
     smoke = bool(input("Do you smoke? \n"))
-    '''
-    Preprocess
-    predict
+    bmi = float(input("What's is your bmi? \n"))
+    region = str(input('Which is your residential area in the US? \n'))
+
+    # Preprocess
+
+    ct = model[-2]
+    scaler = model[-1]
+    x = pd.DataFrame({"age":age, "sex":sex, "bmi":bmi, "child":child, "smoke":smoke," region":region}, index=[0])
+    x_trans = ct.transform(x)
+    x_scaled = scaler.transform(x_trans)
+
+
+    # Predict
+
+    predictions = np.array([clf.predict(x_scaled) for clf in model[:-2]])
     
-    '''
-    print("You are too fucked up 1 milly")
+
+    print(f"Prediction for your conditions: {predictions.mean()}")
--- Chapter 02/07. Gradient Boosting and Encoding/train.py
+++ Chapter 02/07. Gradient Boosting and Encoding/train.py
@@ -1,5 +1,23 @@
 import data_handler as dh
+from sklearn.ensemble import GradientBoostingRegressor
+from xgboost import XGBRegressor
+from sklearn.ensemble import RandomForestRegressor, AdaBoostRegressor
 
-x_train, x_test, y_train, y_test = dh.get_data("./insurance.csv")
 
-print(dh.hello)
+
+def train_model():
+
+    x_train, x_test, y_train, y_test = dh.get_data("C:/Users/andre/Documents/Strive_repository/local_exercise/Chapter 02/07. Gradient Boosting and Encoding/insurance.csv")
+
+    xgb_r = XGBRegressor()
+    gboosting_r = GradientBoostingRegressor(learning_rate=0.01, n_estimators=50)
+    rf = RandomForestRegressor(n_estimators= 50, random_state=0)
+
+    xgb_r.fit(x_train,y_train)
+    gboosting_r.fit(x_train, y_train)
+    rf.fit(x_train, y_train)
+
+    return xgb_r, gboosting_r, rf
+
+train_model()
+
