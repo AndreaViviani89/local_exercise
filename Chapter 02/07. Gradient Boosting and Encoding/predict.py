801343f5111e29f0c89e826fd0a1621441acb91b
Andrea Viviani <andrea.viviani89@gmail.com>
2022-04-20 08:18:09 +0200

    Partial - added pre-process and prediction in the predict.py - need to debug it

--- Chapter 02/07. Gradient Boosting and Encoding/predict.py
+++ Chapter 02/07. Gradient Boosting and Encoding/predict.py
@@ -1,10 +1,13 @@
 import train as tr
 import pandas as pd
 import numpy as np
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OrdinalEncoder, StandardScaler
+
 model = tr.train_model()
 
-ct = model[0]
-print(ct)
+# ct = model[0]
+# print(ct)
 
 while True:
 
@@ -25,23 +28,25 @@ while True:
         smoke = 'no'
 
     bmi = float(input("What's is your bmi? \n"))
-    region = str(input('Which is your residential area in the US? \n'))
+    region = str(input('Which is your residential area in the US? Options: northeast, southeast, southwest, northwest \n'))
 
     # Preprocess
 
-    # x_train = ct.fit_transform(x_train)
-    # x_test = ct.transform(x_test)
-    gbr = model[1]
+    gbr = model[1] # the [1] refers to Gradient Boosting Regressor in the train file
+    scaler = StandardScaler()
 
+    ct = ColumnTransformer( [('ordinal', OrdinalEncoder(handle_unknown= 'use_encoded_value', unknown_value = -1), [1,4,5] )] )
     x = pd.DataFrame({"age":age, "sex":sex, "bmi":bmi, "child":child, "smoke":smoke," region":region}, index=[0])
-
-    x_trans = gbr.transform(x)
-
-
+    x_trans = ct.transform(x)
+    x_scaled = scaler.transform(x_trans)
 
     # Predict
+
+    prediction = (clf.predict(x_scaled) for clf in model[:-2])
+
 
    
     
 
-    print(f"Prediction for your conditions: {predictions.mean()}")
+    print(f"Prediction for your conditions: {prediction.mean()}")
+
