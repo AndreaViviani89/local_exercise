4d021e83509d0aab22e2c99b02cb041daf83949d
Andrea Viviani <andrea.viviani89@gmail.com>
2022-04-22 16:39:13 +0200

    Exercise - Steps on the description
    - split the data
    - apply the enhancement
    - transform the data
    - create a pipeline

--- Chapter 02/11. Data Enhancement/new_feature.py
+++ Chapter 02/11. Data Enhancement/new_feature.py
@@ -103,7 +103,7 @@ def feature_enhancement(data):
             if np.random.randint(2) == 1:
                 df['wind_speed'].values[i] += wind_speed_mean/10
             else:
-                df['wind_spped'].values[i] -= wind_speed_mean/10
+                df['wind_speed'].values[i] -= wind_speed_mean/10
 
             if np.random.randint(2) == 1:
                 df['t1'].values[i] += t1_mean/10
@@ -116,3 +116,102 @@ def feature_enhancement(data):
                 df['t2'].values[i] -= t2_mean/10
 
     return df
+
+
+print(data.head(3))
+gen = feature_enhancement(data)
+print(gen.head(3) )
+
+
+#final_data = data
+y = data['cnt']
+x = data.drop(['cnt'], axis=1)
+
+
+cat_vars = ['season','is_weekend','is_holiday','year','month','weather_code']   # Categorical feature
+num_vars = ['t1','t2','hum','wind_speed']   # Numerical feature
+
+
+x_train, x_val, y_train, y_val = model_selection.train_test_split(x, y,
+                                    test_size=0.2,
+                                    random_state=0  # Recommended for reproducibility
+                                )
+
+
+extra_sample = gen.sample(gen.shape[0] // 3)    # I can change with 4 --> 25%
+x_train = pd.concat([x_train, extra_sample.drop(['cnt'], axis=1 ) ])
+y_train = pd.concat([y_train, extra_sample['cnt'] ])
+
+
+transformer = preprocessing.PowerTransformer()
+y_train = transformer.fit_transform(y_train.values.reshape(-1,1))
+y_val = transformer.transform(y_val.values.reshape(-1,1))
+
+
+rang = abs(y_train.max()) + abs(y_train.min())
+
+
+
+
+num_4_treeModels = pipeline.Pipeline(steps=[
+    ('imputer', impute.SimpleImputer(strategy='constant', fill_value=-9999)),
+])
+
+cat_4_treeModels = pipeline.Pipeline(steps=[
+    ('imputer', impute.SimpleImputer(strategy='constant', fill_value='missing')),
+    ('ordinal', preprocessing.OrdinalEncoder())
+])
+
+
+
+tree_prepro = compose.ColumnTransformer(transformers=[
+    ('num', num_4_treeModels, num_vars),
+    ('cat', cat_4_treeModels, cat_vars),
+], remainder='drop') # Drop other vars not specified in num_vars or cat_vars
+
+
+# dictionary with differet models
+tree_classifiers = {
+  "Decision Tree": DecisionTreeRegressor(),
+  "Extra Trees":   ExtraTreesRegressor(n_estimators=100),
+  "Random Forest": RandomForestRegressor(n_estimators=100),
+  "AdaBoost":      AdaBoostRegressor(n_estimators=100),
+  "Skl GBM":       GradientBoostingRegressor(n_estimators=100),
+  "XGBoost":       XGBRegressor(n_estimators=100),
+  "LightGBM":      LGBMRegressor(n_estimators=100),
+  "CatBoost":      CatBoostRegressor(n_estimators=100),
+}
+
+
+tree_classifiers = {name: pipeline.make_pipeline(tree_prepro, model) for name, model in tree_classifiers.items()}
+
+results = pd.DataFrame({'Model': [], 'MSE': [], 'MAB': [], " % error": [], 'Time': []})
+
+
+for model_name, model in tree_classifiers.items():
+    
+    start_time = time.time()
+    model.fit(x_train, y_train)
+    total_time = time.time() - start_time
+        
+    pred = model.predict(x_val)
+    
+    results = results.append({"Model":    model_name,
+                              "MSE": metrics.mean_squared_error(y_val, pred),
+                              "MAB": metrics.mean_absolute_error(y_val, pred),
+                              " % error": metrics.mean_squared_error(y_val, pred) / rang,
+                              "Time":     total_time},
+                              ignore_index=True)
+
+
+results_ord = results.sort_values(by=['MSE'], ascending=True, ignore_index=True)
+results_ord.index += 1 
+results_ord.style.bar(subset=['MSE', 'MAE'], vmin=0, vmax=100, color='#5fba7d')
+
+print(results_ord)
+
+
+print(y_train.max())
+print(y_train.min())
+print(y_val[3])
+print(tree_classifiers['Random Forest'].predict(x_val)[3])
