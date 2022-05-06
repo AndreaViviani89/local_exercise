87876c4c993ab6bda57dd44973e81534a3149c21
Andrea Viviani <andrea.viviani89@gmail.com>
2022-05-06 16:59:05 +0200

    edit

--- Chapter 02/15. TimeSeries/time_series.py
+++ Chapter 02/15. TimeSeries/time_series.py
@@ -5,6 +5,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 
+from sklearn.ensemble import AdaBoostRegressor, GradientBoostingRegressor
+from catboost import CatBoostRegressor
+from sklearn.svm import SVR
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline, make_pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import mean_absolute_error, r2_score, mean_squared_error
 
 
 # Load the data
@@ -23,7 +30,7 @@ def get_sequence(data, seq_len, target_name):
     for i in range(0, data.shape[0] - (seq_len + 1), seq_len + 1):
 
         seq = data[i : seq_len + i]
-        target = data[target_name][seq_len + 1]
+        target = data[target_name][seq_len + i]
 
         seq_list.append(seq)
         target_list.append(target)
@@ -45,23 +52,70 @@ def get_feature(x):
     for i in range(x.shape[0]):
 
         mean_column_1 = np.mean(x [i, :, 0])                            # calculate the mean for the 1st column
-        std_column_1 = np.std(x [i, : , 0])
-        median_column_1 = np.median(x [i, :, 0])
-        min_column_2 = np.min(x [i, :, 1])
-        mean_column_2 = np.mean(x [i, :, 1])
-        max_column_4 = np.max(x [i, :, 3])
-        std_column_6 = np.std(x [i, :, 5])
-        max_column_6 = np.max(x [i, :, 5])
-        min_column_8 = np.min(x [i, :, 7])
-        mean_column_10 = np.mean(x [i, :, 9])
+        std_column_1 = np.std(x [i, : , 0])                             # calculate the std for the 1st column
+        median_column_1 = np.median(x [i, :, 0])                        # calculate the median for the 1st column
+        min_column_2 = np.min(x [i, :, 1])                              # calculate the min for the column n. 2
+        mean_column_2 = np.mean(x [i, :, 1])                            # calculate the mean for the column n. 2
+        max_column_4 = np.max(x [i, :, 3])                              # calculate the max for the column n. 4
+        std_column_6 = np.std(x [i, :, 5])                              # calculate the std for the column n. 6
+        max_column_6 = np.max(x [i, :, 5])                              # calculate the max for the column n. 6
+        min_column_8 = np.min(x [i, :, 7])                              # calculate the min for the column n. 8
+        mean_column_10 = np.mean(x [i, :, 9])                           # calculate the mean for the column n. 10
 
         # append all the feature in an empty list
         feature.append((mean_column_1, std_column_1, median_column_1, min_column_2, mean_column_2, max_column_4, std_column_6, max_column_6, min_column_8, mean_column_10))
 
     return np.array(feature)
 
-all_the_feature = get_feature(x)
+x = get_feature(x)
 
-print(all_the_feature.shape)
+# print(all_the_feature.shape)
 
-        
+
+# create the models dictionary
+model_dict = {
+                'SVR': SVR(),
+                'Adaboost': AdaBoostRegressor(),
+                'GMB': GradientBoostingRegressor(),
+                'Catboost': CatBoostRegressor(),
+}
+
+# Split the data
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=0, shuffle=False)
+
+# print(x_train.shape)
+# print(x_test.shape)
+# print(y_train.shape)
+# print(y_test.shape)
+
+# Create a pipeline
+pip = {name: Pipeline([('scalar', StandardScaler()), ('regressor', model)]) 
+                       for name, model in model_dict.items()}
+
+
+results = pd.DataFrame({
+                            'Model': [],
+                            'MSE': [],
+                            'MAB': [],
+                            'R2 Score': [],
+                            'Time': []
+})
+
+
+for model_name, model in model_dict.items():
+    start_time = time.time()
+    model.fit(x_train, y_train)
+    finish_time = time.time() - start_time
+
+    pred = model.predict(x_test)
+
+    results = results.append({
+                                    'Model': model_name,
+                                    'MSE': mean_squared_error(y_test, pred),
+                                    'MAB': mean_absolute_error(y_test, pred),
+                                    'R2 Score': r2_score(y_test, pred),
+                                    'Time': finish_time},
+                                    ignore_index=True)
+
+results_sort = results.sort_values(by=['MSE'], ascending= True, ignore_index= True)
+print(results_sort)
