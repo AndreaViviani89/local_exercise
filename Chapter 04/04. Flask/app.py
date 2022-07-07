774010fc94ef0bc0364f55995f8f113065064438
Andrea Viviani <andrea.viviani89@gmail.com>
2022-07-07 16:50:17 +0200

    update

--- Chapter 04/04. Flask/app.py
+++ Chapter 04/04. Flask/app.py
@@ -8,10 +8,19 @@ def index():
     return render_template('home.html')
 
 
+# @app.route("/about") ## Decorator 
 @app.route("/about") ## Decorator 
 def about():
     return render_template('about.html')
 
 
+@app.route("/portfolio")
+def portolio():
+    return render_template('portfolio.html')
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
