4ea91445db12b540105baabc854516a34e81cf5a
Andrea Viviani <andrea.viviani89@gmail.com>
2022-07-18 15:55:04 +0200

    working on the exercise

---
+++ Chapter 04/09. Docker/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+import pickle
+
+
+app = Flask(__name__)
+model = pickle.load(open('model_state.pth'))
+
+@app.route('/', methods=['GET'])
+
+def home():
+    return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+
+def predict():
+    imagefile= request.files['imagefile']
+    image_path= './images/' + imagefile.filename
+    imagefile.save(image_path)
+
+
+
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
